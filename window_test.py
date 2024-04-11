ce1d9f10a71f28e19b3507b45ba5e583b0cdbcde
Jeb Davis <jebemery@gmail.com>
2024-04-11 11:00:32 -0600

    Update window_test.py

--- window_test.py
+++ window_test.py
@@ -1,23 +1,19 @@
 import pygame
 from pygame.locals import *
+pygame.init()
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((200, 200),RESIZABLE|SCALED)
-    fake_screen = screen.copy()
-    pic = pygame.surface.Surface((50, 50))
-    pic.fill((255, 100, 200))
+info = pygame.display.Info()
+w = info.current_w
+h = info.current_h
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT: 
-                pygame.display.quit()
-            elif event.type == VIDEORESIZE:
-                screen = pygame.display.set_mode(event.size, HWSURFACE|DOUBLEBUF|RESIZABLE)
+screen = pygame.display.set_mode((w, h-55), SCALED)
 
-        fake_screen.fill('black')
-        fake_screen.blit(pic, (100, 100))
-        screen.blit(pygame.transform.scale(fake_screen, screen.get_rect().size), (0, 0))
-        pygame.display.flip()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT: 
+            pygame.display.quit()
+        
     
-main()    
+        
